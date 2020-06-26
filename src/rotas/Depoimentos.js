e475f5f52fe65afb5b0cceed8b8dd0de41deaa71
gusleaooliveira <gus.leaono@gmail.com>
2020-06-25 21:52:33 -0300

    alteração feita em:25/06/2020-21:52:33

--- src/rotas/Depoimentos.js
+++ src/rotas/Depoimentos.js
@@ -10,7 +10,9 @@ const collection = "depoimento";
 
 const cliente = new MongoClient(url);
 
-router.use(express.bodyParser());
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+router.use(bodyParser.raw());
 router.use(cors());
 
 router.use((req, res, next) => {
