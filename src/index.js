641d4bb0a8ba22eafce115ea37a332bfb3bc090c
gusleaooliveira <gus.leaono@gmail.com>
2020-06-27 16:57:09 -0300

    alteração feita em:27/06/2020-16:57:09

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const rotaDepoimentos = require("./rotas/Depoimentos");
+const rotaContatos = require("./rotas/Contatos");
 const app = express();
 
 
 app.use(rotaDepoimentos);
+app.use(rotaContatos);
 
 app.listen(process.env.PORT || 5000);
